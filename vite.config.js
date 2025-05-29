2735077080109e2e0bcfb623b240c546773bca5b
juan-moresi <jumor701@gmail.com>
2025-05-28 23:44:32 -0300

    navegalasRutas-moresi

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    sourcemap: false, // Establece esto en 'false' para deshabilitar los mapas de origen en producción
+  _plugins: [react()],
+  get plugins() {
+    return this._plugins
+  },
+  set plugins(value) {
+    this._plugins = value
   },
 })
